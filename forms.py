bbac9754d1ae6586c1a0b0879f676c12b98d0ce8
Malcolm Leehan <kx1863tg@go.minneapolis.edu>
2017-02-05 18:37:47 -0600

    Delete forms.py

--- forms.py
+++
@@ -1,7 +0,0 @@
-from flask_wtf import Form
-from wtforms.fields.html5 import DateField
-from wtforms.widgets import RadioInput
-
-
-class DateForm(Form):
-    dt = DateField('DatePicker', format='%y-%m-%d')
